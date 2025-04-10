137642b27ba0e7e2c178d1bc124a78e1563d21a3
Renyigesai <2907654918@qq.com>
2025-04-10 09:40:29 +0800

    修复了酒持续时长bug、文化底蕴卡附魔台等级bug

---
+++ src/main/java/com/renyigesai/immortalers_delight/api/event/SnifferDropSeedEvent.java
@@ -0,0 +1,25 @@
+package com.renyigesai.immortalers_delight.api.event;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.eventbus.api.Event;
+
+
+public class SnifferDropSeedEvent extends Event {
+    private final Level level;
+    private final BlockPos blockPos;
+
+    public SnifferDropSeedEvent(Level level, BlockPos blockPos) {
+        this.level = level;
+        this.blockPos = blockPos;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public BlockPos getBlockPos() {
+        return blockPos;
+    }
+}
