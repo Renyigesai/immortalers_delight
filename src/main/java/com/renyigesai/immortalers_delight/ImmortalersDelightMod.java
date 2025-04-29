16fa406eddcb66b1f3a716f98e555a45281d1743
Renyigesai <2907654918@qq.com>
2025-04-29 21:10:09 +0800

    随便的提交?)2025/04/29

--- src/main/java/com/renyigesai/immortalers_delight/ImmortalersDelightMod.java
+++ src/main/java/com/renyigesai/immortalers_delight/ImmortalersDelightMod.java
@@ -38,7 +38,6 @@ public class ImmortalersDelightMod {
 
     public static final String MODID = "immortalers_delight";
     public static final Logger LOGGER = LogUtils.getLogger();
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
     public ImmortalersDelightMod() {
         MinecraftForge.EVENT_BUS.register(this);
@@ -100,8 +99,6 @@ public class ImmortalersDelightMod {
             AftertasteHungerOverlay.init();
             KeepFastHungerOverlay.init();
             BurnTheBoatsHealthOverlay.init();
-
-            //GasPoisonHealthOverlay.init();
         }
     }
 }
