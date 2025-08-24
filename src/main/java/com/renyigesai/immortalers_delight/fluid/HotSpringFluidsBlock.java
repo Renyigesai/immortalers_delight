85b27eda96867a12a7b63f1015d97821ecc38167
Renyigesai <2907654918@qq.com>
2025-08-24 19:18:41 +0800

    修复战利品表相关问题2025/08/24

--- src/main/java/com/renyigesai/immortalers_delight/fluid/HotSpringFluidsBlock.java
+++ src/main/java/com/renyigesai/immortalers_delight/fluid/HotSpringFluidsBlock.java
@@ -2,8 +2,15 @@ package com.renyigesai.immortalers_delight.fluid;
 
 import com.renyigesai.immortalers_delight.recipe.EnchantalCoolerRecipe;
 import com.renyigesai.immortalers_delight.recipe.HotSpringRecipe;
+import com.renyigesai.immortalers_delight.util.ItemUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.Entity;
@@ -17,12 +24,10 @@ import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.event.sound.SoundEvent;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 //待优化
 public class HotSpringFluidsBlock extends LiquidBlock {
@@ -35,10 +40,10 @@ public class HotSpringFluidsBlock extends LiquidBlock {
 
 
     private Optional<HotSpringRecipe> getCurrentRecipe(Level level, List<ItemStack> list) {
-        SimpleContainer inventory = new SimpleContainer(4);
+        SimpleContainer inventory = new SimpleContainer(6);
         List<ItemStack> inputs = new ArrayList<>();
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < list.size(); i++) {
             ItemStack stack = list.get(i);
             if (!stack.isEmpty()) {
                 inputs.add(stack);
@@ -53,40 +58,64 @@ public class HotSpringFluidsBlock extends LiquidBlock {
                 .getRecipeFor(HotSpringRecipe.Type.INSTANCE, inventory, level);
     }
 
-//    @Override
-//    public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
-//        super.randomTick(pState, pLevel, pPos, pRandom);
-//        craftTick(pLevel,pPos);
-//    }
-
     @Override
-    public boolean isRandomlyTicking(@NotNull BlockState pState) {
-        return true;
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, RandomSource pRandom) {
+        super.animateTick(pState, pLevel, pPos, pRandom);
+        if (isHeatSources(pLevel,pPos)){
+            double x = pPos.getX();
+            double y = pPos.above().getY();
+            double z = pPos.getZ();
+            Random random = new Random();
+            pLevel.addParticle(ParticleTypes.POOF,x + random.nextDouble(-0.5,0.5),y,z + random.nextDouble(-0.5,0.5),0,0,0);
+        }
     }
 
-    private void craftTick(Level level,BlockPos pos){
-        Vec3 vec3 = new Vec3(pos.getX(),pos.getY(),pos.getZ());
-//        List<ItemEntity> itemEntityList = level.getEntitiesOfClass(ItemEntity.class, new AABB(vec3, vec3).inflate(1 / 2d), e
-//                -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(vec3))).toList();
-//        System.out.println(itemEntityList);
-        List<Entity> entities = level.getEntitiesOfClass(Entity.class, new AABB(vec3, vec3).inflate(1 / 2d), e
-                -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(vec3))).toList();
-        System.out.println(entities);
-//        if (itemEntityList.isEmpty())
-//            return;
-//        List<ItemStack> stackList = new ArrayList<>();
-//        for (ItemEntity item : itemEntityList) {
-//            if (!item.getItem().isEmpty()) {
-//                stackList.add(item.getItem());
-//            }
-//        }
-//        if (stackList.isEmpty())
-//            return;
-//        Optional<HotSpringRecipe> recipeOptional = getCurrentRecipe(level,stackList);
-//        if (recipeOptional.isEmpty()){
-//            return;
-//        }
-//        System.out.println("Yes");
+    @Override
+    public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
+        craftTick(pLevel,pPos);
+    }
 
+    public boolean isHeatSources(Level level, BlockPos pos){
+        return level.getBlockState(pos.below()).is(BlockTags.create(new ResourceLocation("farmersdelight:heat_sources")));
+    }
+
+    private void craftTick(Level level, BlockPos pos){
+        List<ItemEntity> itemEntityList = level.getEntitiesOfClass(ItemEntity.class, new AABB(pos,pos).inflate(1,1,1));
+        if (itemEntityList.isEmpty())
+            return;
+        List<ItemStack> stackList = new ArrayList<>();
+        for (int i = 0; i < itemEntityList.size(); i++) {
+            ItemStack item = itemEntityList.get(i).getItem();
+            if (!item.isEmpty() && item.getCount() == 1) {
+                stackList.add(i,item);
+            }
+        }
+        if (stackList.isEmpty())
+            return;
+        Optional<HotSpringRecipe> recipeOptional = getCurrentRecipe(level,stackList);
+        if (recipeOptional.isEmpty()){
+            return;
+        }
+        HotSpringRecipe recipe = recipeOptional.get();
+        ItemStack resultItem = recipe.getResultItem(level.registryAccess()).copy();
+        for (int i = 0; i < itemEntityList.size(); i++) {
+            ItemEntity itemEntity = itemEntityList.get(i);
+            itemEntity.remove(Entity.RemovalReason.DISCARDED);
+        }
+        ItemUtils.spawnItemEntity(level,resultItem,pos.getX()+0.5,pos.getY()+0.5,pos.getZ()+0.5,0,0,0);
+        if (level instanceof ServerLevel serverLevel){
+            serverLevel.sendParticles(ParticleTypes.POOF,pos.getX()+0.5,pos.getY()+0.5,pos.getZ()+0.5,8,0.0,0.0,0.0,0);
+            level.playSound(null,pos,SoundEvents.FIRE_EXTINGUISH,SoundSource.BLOCKS);
+        }
+    }
+
+    @Override
+    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        super.entityInside(pState, pLevel, pPos, pEntity);
+        if (!isHeatSources(pLevel,pPos))
+            return;
+        if (pEntity instanceof ItemEntity){
+            pLevel.scheduleTick(pPos,this,5);
+        }
     }
 }
