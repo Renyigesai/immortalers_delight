afb5166c3d7f0d984c450a31390d7ea435a223d1
renyigesai <2907654918@qq.com>
2026-05-19 11:59:08 +0800

    将部分tag变更为c：2026/05/19

--- src/main/java/com/renyigesai/immortalers_delight/client/ClientEventHelper.java
+++ src/main/java/com/renyigesai/immortalers_delight/client/ClientEventHelper.java
@@ -1,44 +1,35 @@
-package com.renyigesai.immortalers_delight.client;
-import net.neoforged.fml.common.EventBusSubscriber;
-
-import com.renyigesai.immortalers_delight.ImmortalersDelightMod;
-import com.renyigesai.immortalers_delight.client.renderer.special_item.ItemTESRenderer;
-import com.renyigesai.immortalers_delight.init.ImmortalersDelightItems;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
-import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
-
-@OnlyIn(Dist.CLIENT)
-@EventBusSubscriber(modid = ImmortalersDelightMod.MODID, value = Dist.CLIENT)
-public class ClientEventHelper {
-
-    @SubscribeEvent
-    public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
-        IClientItemExtensions specialItemRenderer = new IClientItemExtensions() {
-            @Override
-            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
-                return new ItemTESRenderer(
-                        Minecraft.getInstance().getBlockEntityRenderDispatcher(),
-                        Minecraft.getInstance().getEntityModels());
-            }
-        };
-        event.registerItem(
-                specialItemRenderer,
-                ImmortalersDelightItems.LARGE_COLUMN.get(),
-                ImmortalersDelightItems.JENG_NANU.get(),
-                ImmortalersDelightItems.BONE_KNIFE.get());
-    }
-
-//    @SubscribeEvent
-//    public void onPostRenderLiving(RenderLivingEvent.Post event) {
-//        LivingEntity entity = event.getEntity();
+//package com.renyigesai.immortalers_delight.client;
+//import net.neoforged.fml.common.EventBusSubscriber;
 //
-//        if(data!=null&&data.getFrozen()){
-//            IceRenderer.render(event.getEntity(), event.getPoseStack(), event.getMultiBufferSource(), event.getPackedLight(), data.getFrozenTick());
-//        }
+//import com.renyigesai.immortalers_delight.ImmortalersDelightMod;
+//import com.renyigesai.immortalers_delight.client.renderer.special_item.ItemTESRenderer;
+//import com.renyigesai.immortalers_delight.init.ImmortalersDelightItems;
+//import net.minecraft.client.Minecraft;
+//import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+//import net.neoforged.api.distmarker.Dist;
+//import net.neoforged.api.distmarker.OnlyIn;
+//import net.neoforged.bus.api.SubscribeEvent;
+//import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+//import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
+//
+//@OnlyIn(Dist.CLIENT)
+//@EventBusSubscriber(modid = ImmortalersDelightMod.MODID, value = Dist.CLIENT)
+//public class ClientEventHelper {
+//
+//    @SubscribeEvent
+//    public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
+//        IClientItemExtensions specialItemRenderer = new IClientItemExtensions() {
+//            @Override
+//            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+//                return new ItemTESRenderer(
+//                        Minecraft.getInstance().getBlockEntityRenderDispatcher(),
+//                        Minecraft.getInstance().getEntityModels());
+//            }
+//        };
+//        event.registerItem(
+//                specialItemRenderer,
+//                ImmortalersDelightItems.LARGE_COLUMN.get(),
+//                ImmortalersDelightItems.JENG_NANU.get(),
+//                ImmortalersDelightItems.BONE_KNIFE.get());
 //    }
-}
+//}
